52416e2a456b46c51f7af34713100d5f7e920097
Matahari Ramadhan <ramadhan.matahari24@gmail.com>
2022-10-26 16:50:31 +0200

    implement get all books controller

--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -1,12 +1,24 @@
 package controllers
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/matahariramadhan/go-book-management-api/pkg/models"
+)
+
+var Book *models.Book
 
 func CreateBook(w http.ResponseWriter, r *http.Request) {
 
 }
-func GetAllBooks(w http.ResponseWriter, r *http.Request) {
 
+func GetAllBooks(w http.ResponseWriter, r *http.Request) {
+	books := models.FindAllBooks()
+	res, _ := json.Marshal(books)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
 }
 func GetBookById(w http.ResponseWriter, r *http.Request) {
 
