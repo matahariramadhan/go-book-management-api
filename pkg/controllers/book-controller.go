e52050d9ab5e08ef64fac5c9baf6d95a7b8e98f9
Matahari Ramadhan <ramadhan.matahari24@gmail.com>
2022-10-26 13:28:20 +0200

    Done implement routes, add boilerplate to controller

---
+++ pkg/controllers/book-controller.go
@@ -0,0 +1,19 @@
+package controllers
+
+import "net/http"
+
+func CreateBook(w http.ResponseWriter, r *http.Request) {
+
+}
+func GetAllBooks(w http.ResponseWriter, r *http.Request) {
+
+}
+func GetBookById(w http.ResponseWriter, r *http.Request) {
+
+}
+func UpdateBook(w http.ResponseWriter, r *http.Request) {
+
+}
+func DeleteBook(w http.ResponseWriter, r *http.Request) {
+
+}
--- pkg/routes/book-routes.go
+++ pkg/routes/book-routes.go
@@ -2,12 +2,13 @@ package routes
 
 import (
 	"github.com/gorilla/mux"
+	"github.com/matahariramadhan/go-book-management-api/pkg/controllers"
 )
 
 var RegisterBookRoutes = func(router *mux.Router) {
-	router.HandleFunc("/book", createBook).Methods("POST")
-	router.HandleFunc("/book", getAllBooks).Methods("GET")
-	router.HandleFunc("/book/{bookId}", getBookById).Methods("GET")
-	router.HandleFunc("/book/{bookId}", updateBook).Methods("PUT")
-	router.HandleFunc("/book/{bookId}", deleteBook).Methods("DELETE")
+	router.HandleFunc("/book", controllers.CreateBook).Methods("POST")
+	router.HandleFunc("/book", controllers.GetAllBooks).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.GetBookById).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.UpdateBook).Methods("PUT")
+	router.HandleFunc("/book/{bookId}", controllers.DeleteBook).Methods("DELETE")
 }
